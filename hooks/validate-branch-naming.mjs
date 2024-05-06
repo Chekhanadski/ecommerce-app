67361f4e6b061d20df6d4c5f301885f748751d05
mikhailau-pavel <119894065+mikhailau-pavel@users.noreply.github.com>
2024-05-06 19:38:44 +0300

    feat: add missing prefix for branch name validation hook (#27)

--- hooks/validate-branch-naming.mjs
+++ hooks/validate-branch-naming.mjs
@@ -5,7 +5,7 @@ exec('git rev-parse --abbrev-ref HEAD', (err, stdout) => {
     throw new Error(err);
   }
 
-  if (!/RSS-ECOMM-[0-9]+_[0-9-]+[a-zA-Z]+/.test(stdout.trim())) {
+  if (!/(bugfix|feat|hotfix|refactor|test)\/RSS-ECOMM-[0-9]+_[0-9-]+[a-zA-Z]+/.test(stdout.trim())) {
     throw new Error('The branch name is not valid!');
   }
 });
