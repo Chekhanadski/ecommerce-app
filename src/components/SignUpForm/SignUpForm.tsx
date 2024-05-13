8aff9a9d57cb62b0bf818d18573a21507c6d7595
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-05-13 23:41:05 +0200

    feat: add check password

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -5,12 +5,17 @@ import './styles.css';
 function SignUpForm(): React.ReactElement {
   const [dateFocused, setDateFocused] = useState(false);
   const [country, setCountry] = useState('');
+
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
 
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
   const validateEmail = (emailString: string) => {
     const re =
-      /^(([^<>()\\.,;:@"]+(\.[^<>()\\.,;:@"+]*)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      /^(([^<>()\\.,;:@"]+(\.[^<>()\\.,;:@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     return re.test(emailString.toLowerCase());
   };
 
@@ -23,6 +28,31 @@ function SignUpForm(): React.ReactElement {
     }
   };
 
+  const validatePassword = (passwordString: string) => {
+    if (!/^[A-Za-z0-9!@#$%^&*()_+\-={};':"\\|,.<>?]+$/.test(passwordString)) {
+      return 'Password can only contain English letters, digits, and special chars.';
+    }
+    if (!/[A-Z]/.test(passwordString)) {
+      return 'Password must contain at least one uppercase letter';
+    }
+    if (!/[a-z]/.test(passwordString)) {
+      return 'Password must contain at least one lowercase letter';
+    }
+    if (!/[0-9]/.test(passwordString)) {
+      return 'Password must contain at least one digit';
+    }
+    if (passwordString.length < 8) {
+      return 'Password must be at least 8 characters long';
+    }
+    return '';
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+    const error = validatePassword(event.target.value);
+    setPasswordError(error);
+  };
+
   const handleCountryChange = (event: ChangeEvent<HTMLSelectElement>) => {
     setCountry(event.target.value);
   };
@@ -32,37 +62,56 @@ function SignUpForm(): React.ReactElement {
       className="sign-up-form"
       onSubmit={(e) => {
         e.preventDefault(); /* your submit logic here */
-      }}>
+      }}
+    >
       <span>Enter your details below</span>
       <div className="input-container">
         <input id="email" type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
         {emailError && <div className="error">{emailError}</div>}
       </div>
-      <input type="password" placeholder="Password" />
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Surname" />
-      <input
-        type={dateFocused ? 'date' : 'text'}
-        placeholder="Date of Birth"
-        onFocus={() => setDateFocused(true)}
-        onBlur={() => setDateFocused(false)}
-      />
-      <div className="form-address">
-        <span>Address:</span>
+      <div className="input-container">
+        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+        {passwordError && <div className="error">{passwordError}</div>}
+      </div>
+      <div className="input-container">
+        <input type="text" placeholder="Name" />
+      </div>
+      <div className="input-container">
+        <input type="text" placeholder="Surname" />
+      </div>
+      <div className="input-container">
+        <input
+          type={dateFocused ? 'date' : 'text'}
+          placeholder="Date of Birth"
+          onFocus={() => setDateFocused(true)}
+          onBlur={() => setDateFocused(false)}
+        />
+      </div>
+      <span>Your Address:</span>
+
+      <div className="input-container">
         <input type="text" placeholder="Street" />
+      </div>
+      <div className="input-container">
         <input type="text" placeholder="City" />
+      </div>
+      <div className="input-container">
         <input type="text" placeholder="Post code" />
+      </div>
+      <div className="input-container">
         <select
           name="country"
           id="country"
           value={country}
           onChange={handleCountryChange}
-          className={country ? '' : 'placeholder'}>
+          className={country ? '' : 'placeholder'}
+        >
           <option value="">Select Country</option>
           <option value="USA">USA</option>
           <option value="Germany">Germany</option>
         </select>
       </div>
+
       <Button onClick={() => () => {}}>Create Account</Button>
     </form>
   );
