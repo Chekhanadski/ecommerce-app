9117ace3faafdb91550306b5fdcb93c6925f5c8a
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-05-13 20:11:53 +0200

    feat: add check email

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,22 +1,67 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import Button from '../Button/Button';
 import './styles.css';
 
 function SignUpForm(): React.ReactElement {
+  const [dateFocused, setDateFocused] = useState(false);
+  const [country, setCountry] = useState('');
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const validateEmail = (emailString: string) => {
+    const re =
+      /^(([^<>()\\.,;:@"]+(\.[^<>()\\.,;:@"+]*)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(emailString.toLowerCase());
+  };
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+    if (!validateEmail(event.target.value)) {
+      setEmailError('Invalid email format');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const handleCountryChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setCountry(event.target.value);
+  };
+
   return (
-    <form className="sign-up-form">
+    <form
+      className="sign-up-form"
+      onSubmit={(e) => {
+        e.preventDefault(); /* your submit logic here */
+      }}>
       <span>Enter your details below</span>
-      <input id="email" type="email" placeholder="Email" />
+      <div className="input-container">
+        <input id="email" type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+        {emailError && <div className="error">{emailError}</div>}
+      </div>
       <input type="password" placeholder="Password" />
       <input type="text" placeholder="Name" />
       <input type="text" placeholder="Surname" />
-      <input type="date" placeholder="Date of Birth" />
+      <input
+        type={dateFocused ? 'date' : 'text'}
+        placeholder="Date of Birth"
+        onFocus={() => setDateFocused(true)}
+        onBlur={() => setDateFocused(false)}
+      />
       <div className="form-address">
         <span>Address:</span>
         <input type="text" placeholder="Street" />
         <input type="text" placeholder="City" />
         <input type="text" placeholder="Post code" />
-        <input type="text" placeholder="Country" />
+        <select
+          name="country"
+          id="country"
+          value={country}
+          onChange={handleCountryChange}
+          className={country ? '' : 'placeholder'}>
+          <option value="">Select Country</option>
+          <option value="USA">USA</option>
+          <option value="Germany">Germany</option>
+        </select>
       </div>
       <Button onClick={() => () => {}}>Create Account</Button>
     </form>
