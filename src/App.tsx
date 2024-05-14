88fb2fd4e7dc7d051dd6e20cfb76f6239bbbd163
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-05-14 17:30:10 +0200

    refactor: move Header and Footer to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
 
 function App(): React.ReactElement {
   return (
     <div>
       <div className="sale-offer"> </div>
-      <RegistrationPage />
+      <div className="wrapper">
+        <Header />
+        <RegistrationPage />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import Footer from '../../components/Footer/Footer';
-import Header from '../../components/Header/Header';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
-import './styles.css';
+import styles from './styles.module.css';
 
 function RegistrationPage() {
   return (
-    <div className="wrapper">
-      <Header />
-
-      <main className="main-block">
+    <main className={styles.mainBlock}>
+      <div>
+        <img src="/img/main-img.jpg" alt="Main" />
+      </div>
+      <div className={styles.formSignUpBlock}>
         <div>
-          <img src="/img/main-img.jpg" alt="Main" />
+          <h1 className={styles.h1}>Create an account</h1>
+          <SignUpForm />
         </div>
-        <div className="form-sign-up-block">
-          <div>
-            <h1 className="h1">Create an account</h1>
-            <SignUpForm />
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
+      </div>
+    </main>
   );
 }
 
