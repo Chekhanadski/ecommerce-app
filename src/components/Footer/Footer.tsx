33bf0380b9970c61e7e56ef6a5fc1181c1563aa4
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-05-15 23:26:48 +0200

    feat: create responsive layout for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,11 @@ function Footer() {
   return (
     <footer className={styles.footerBlock}>
       <img className={styles.rsschoolLogo} src="/img/rsschool-logo.png" alt="RSSchool Logo" />
-      <p className={styles.footerText}>Final project within the JavaScript / Front-end Course RSSchool in 2024.</p>
+      <div>
+        <p className={styles.footerText}>eCommerce project 2024.</p>
+      </div>
+      <img className={styles.exclusiveLogo} src="/img/logo-exclusive-white.png" alt="RSSchool Logo" />
+      
     </footer>
   );
 }
