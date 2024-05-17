c5ceed9b0971cde3ad7dd58b041e64fad28e4b58
v-m-dev <morozvitalii2403@gmail.com>
2024-05-17 14:41:50 +0300

    feat: add login page

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import Button from '../Button/Button';
 import styles from './styles.module.css';
 import * as regexps from '../../constants/regexps';
@@ -10,28 +10,13 @@ interface FormData {
 }
 
 export default function LoginForm(): React.ReactElement {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    control
-  } = useForm<FormData>({
+  const { register, handleSubmit } = useForm<FormData>({
     mode: 'all',
     defaultValues: {
       email: '',
-      password: '',
-      name: '',
-      surname: '',
-      dob: '',
-      street: '',
-      city: '',
-      postcode: '',
-      country: ''
+      password: ''
     }
   });
-
-  const [dobActivated, setDobActivated] = useState(false);
-  const handleDobFocus = () => setDobActivated(true);
 
   const validateEmail = (emailString: string) => {
     if (emailString.length && !regexps.emailRegexp.test(emailString.toLowerCase())) {
@@ -65,77 +50,6 @@ export default function LoginForm(): React.ReactElement {
     return undefined;
   };
 
-  const validateName = (nameString: string) => {
-    if (nameString.length && !regexps.nameRegexp.test(nameString)) {
-      return 'Name can only contain English letters';
-    }
-    if (nameString.length < 1) {
-      return 'Name is required';
-    }
-    return undefined;
-  };
-
-  const validateSurname = (surnameString: string) => {
-    if (surnameString.length < 1) {
-      return 'Surname is required';
-    }
-    if (!regexps.nameRegexp.test(surnameString)) {
-      return 'Surname can only contain English letters';
-    }
-    return undefined;
-  };
-
-  const validateDob = (dobString: string) => {
-    const dobDate = new Date(dobString);
-    const currentDate = new Date();
-    let age = currentDate.getFullYear() - dobDate.getFullYear();
-    const m = currentDate.getMonth() - dobDate.getMonth();
-    if (m < 0 || (m === 0 && currentDate.getDate() < dobDate.getDate())) {
-      age -= 1;
-    }
-    if (age < 13) {
-      return 'You must be at least 13 years old';
-    }
-    if (dobString.length < 1) {
-      return 'Date of Birth is required';
-    }
-    return true;
-  };
-
-  const validateStreet = (streetString: string) => {
-    if (streetString.length < 1) {
-      return 'Street is required';
-    }
-    return undefined;
-  };
-
-  const validateCity = (cityString: string) => {
-    if (cityString.length < 1) {
-      return 'City is required';
-    }
-    if (!regexps.cityRegexp.test(cityString)) {
-      return 'City can only contain English letters and spaces';
-    }
-    return undefined;
-  };
-
-  const validatePostcode = (postcodeString: string) => {
-    if (postcodeString.length < 1) {
-      return 'Postcode is required';
-    }
-    if (!regexps.postcodeRegexp.test(postcodeString)) {
-      return 'Postcode must consist of 5 digits';
-    }
-    return undefined;
-  };
-
-  const validateCountry = (countryString: string) => {
-    if (countryString === '') {
-      return 'Country is required';
-    }
-    return true;
-  };
-
   const onSubmit = (data: FormData) => data;
 
   return (
@@ -144,71 +58,13 @@ export default function LoginForm(): React.ReactElement {
 
       <div className={styles.inputContainer}>
         <input type="email" placeholder="Email" {...register('email', { validate: validateEmail })} />
-        {errors.email && <div className={styles.error}>{errors.email.message}</div>}
       </div>
 
       <div className={styles.inputContainer}>
         <input type="password" placeholder="Password" {...register('password', { validate: validatePassword })} />
-        {errors.password && <div className={styles.error}>{errors.password.message}</div>}
       </div>
 
-      <div className={styles.inputContainer}>
-        <input placeholder="Name" {...register('name', { validate: validateName })} />
-        {errors.name && <div className={styles.error}>{errors.name.message}</div>}
-      </div>
-
-      <div className={styles.inputContainer}>
-        <input placeholder="Surname" {...register('surname', { validate: validateSurname })} />
-        {errors.surname && <div className={styles.error}>{errors.surname.message}</div>}
-      </div>
-
-      <div className={styles.inputContainer}>
-        <input
-          type={dobActivated ? 'date' : 'text'}
-          placeholder={!dobActivated ? 'Date of Birth' : undefined}
-          onFocus={handleDobFocus}
-          {...register('dob', { validate: validateDob })}
-        />
-        {errors.dob && <div className={styles.error}>{errors.dob.message}</div>}
-      </div>
-
-      <span>Your Address:</span>
-
-      <div className={styles.inputContainer}>
-        <input placeholder="Street" {...register('street', { validate: validateStreet })} />
-        {errors.street && <div className={styles.error}>{errors.street.message}</div>}
-      </div>
-
-      <div className={styles.inputContainer}>
-        <input placeholder="City" {...register('city', { validate: validateCity })} />
-        {errors.city && <div className={styles.error}>{errors.city.message}</div>}
-      </div>
-
-      <div className={styles.inputContainer}>
-        <input placeholder="Post code" {...register('postcode', { validate: validatePostcode })} />
-        {errors.postcode && <div className={styles.error}>{errors.postcode.message}</div>}
-      </div>
-
-      <div className={styles.inputContainer}>
-        <Controller
-          name="country"
-          control={control}
-          defaultValue=""
-          rules={{ validate: validateCountry }}
-          render={({ field }) => (
-            <select {...field}>
-              <option value="" disabled hidden>
-                Select Country
-              </option>
-              <option value="Germany">Germany</option>
-              <option value="USA">USA</option>
-            </select>
-          )}
-        />
-        {errors.country && <div className={styles.error}>{errors.country.message}</div>}
-      </div>
-
-      <Button type="submit">Create Account</Button>
+      <Button type="submit">Login</Button>
     </form>
   );
 }
