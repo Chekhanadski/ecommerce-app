8742317c2db6435027e4b722eb6d47bdbc4900cc
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-05-27 19:54:56 +0200

    feat: create display prices with and without discount

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,37 +1,57 @@
 import { IoCartOutline } from 'react-icons/io5';
 import React, { useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import { ProductData } from '../../store/types/products';
 import styles from './styles.module.css';
 
 function ProductCard({ product }: { product: ProductData }) {
-  const { productName, productDescription, productImage } = useMemo(() => {
+  const { productId, productName, productDescription, productImage, fullPrice, discountedPrice } = useMemo(() => {
+    if (!product.masterData || !product.masterData.current.masterVariant.prices[0]) {
+      return {};
+    }
+
+    const productId = product.id;
     const name = product.masterData.current.name['en-US'];
     const description = product.masterData.current.description['en-US'];
     const image = product.masterData.current.masterVariant.images[0].url;
+    const price = product.masterData.current.masterVariant.prices[0];
+    const fullPrice = price.value.centAmount / 100;
+
+    const discountedPrice = price.discounted ? price.discounted.value.centAmount / 100 : undefined;
 
     function truncate(str: string, num: number) {
       return str.length > num ? `${str.slice(0, num)}...` : str;
     }
 
     return {
+      productId,
       productName: name,
       productDescription: truncate(description, 150),
-      productImage: image
+      productImage: image,
+      fullPrice,
+      discountedPrice
     };
   }, [product]);
 
   return (
     <div className={styles.productCard}>
-      <img className={styles.productImg} src={productImage} alt={productName} />
-      <div className={styles.descriptionBlock}>
-        <p>{productDescription}</p>
-      </div>
-      <div className={styles.productCartBlock}>
+      <Link className={styles.linkCard} to={`/catalog/${productId}`}>
+        <img className={styles.productImg} src={productImage} alt={productName} />
+        <div className={styles.descriptionBlock}>
+          <p>{productDescription}</p>
+        </div>
+      </Link>
+      <button type="button" className={styles.productCartBlock}>
         <IoCartOutline size={25} />
         <span>Add To Cart</span>
-      </div>
-      <h3>{productName}</h3>
-      <div className={styles.priceBlock}>Price:</div>
+      </button>
+      <Link className={styles.linkCard} to={`/catalog/${productId}`}>
+        <h3>{productName}</h3>
+        <div className={styles.priceBlock}>
+          {discountedPrice && <div className={styles.discountedPrice}>{discountedPrice}€</div>}
+          <div className={discountedPrice ? styles.priceStriked : styles.price}>{`${fullPrice}€`}</div>
+        </div>
+      </Link>
     </div>
   );
 }
