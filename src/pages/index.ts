132327a392c0e630a276ea121609de615589cadc
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-05-28 18:07:59 +0200

    refactor: create file index.ts with imports

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { default as LoginPage } from './LoginPage/LoginPage';
+export { default as AccountPage } from './AccountPage/AccountPage';
+export { default as AddressPage } from './AddressPage/AddressPage';
+export { default as CatalogPage } from './CatalogPage/CatalogPage';
+export { default as MainPage } from './MainPage/MainPage';
+export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
+export { default as RegistrationPage } from './RegistrationPage/RegistrationPage';
--- src/router/MainSectionRouter/MainSectionRouter.tsx
+++ src/router/MainSectionRouter/MainSectionRouter.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RegistrationPage from '../../pages/RegistrationPage/RegistrationPage';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
-import LoginPage from '../../pages/LoginPage/LoginPage';
-import MainPage from '../../pages/MainPage/MainPage';
-import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
-import CatalogPage from '../../pages/CatalogPage/CatalogPage';
-import AccountPage from '../../pages/AccountPage/AccountPage';
-import AddressPage from '../../pages/AddressPage/AddressPage';
+import {
+  LoginPage,
+  MainPage,
+  AccountPage,
+  AddressPage,
+  CatalogPage,
+  RegistrationPage,
+  NotFoundPage
+} from '../../pages';
 
 function MainSectionRouter() {
   return (
