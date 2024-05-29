99c25ba5a782b604680b7e61b80862636a7f7794
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-05-29 10:09:55 +0200

    refactor: refactor MainSectionRouter

--- src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as LoginPage } from './LoginPage/LoginPage';
-export { default as AccountPage } from './AccountPage/AccountPage';
-export { default as AddressPage } from './AddressPage/AddressPage';
-export { default as CatalogPage } from './CatalogPage/CatalogPage';
-export { default as MainPage } from './MainPage/MainPage';
-export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
-export { default as RegistrationPage } from './RegistrationPage/RegistrationPage';
--- src/router/MainSectionRouter/MainSectionRouter.tsx
+++ src/router/MainSectionRouter/MainSectionRouter.tsx
@@ -2,34 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-
-import {
-  LoginPage,
-  MainPage,
-  AccountPage,
-  AddressPage,
-  CatalogPage,
-  RegistrationPage,
-  NotFoundPage
-} from '../../pages/index';
-
-const routes = [
-  { path: '/', element: <MainPage /> },
-  { path: '/catalog', element: <CatalogPage /> },
-  { path: '/login', element: <LoginPage /> },
-  { path: '/register', element: <RegistrationPage /> },
-  { path: '/account', element: <AccountPage /> },
-  { path: '/account/address', element: <AddressPage /> },
-  { path: '*', element: <NotFoundPage /> }
-];
+import ROUTES from '../../pages/routes';
 
 function MainSectionRouter() {
   return (
     <Router>
       <Header />
       <Routes>
-        {routes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
+        {ROUTES.map((route) => (
+          <Route key={route.path} path={route.path} element={<route.element/>} />
         ))}
       </Routes>
       <Footer />
