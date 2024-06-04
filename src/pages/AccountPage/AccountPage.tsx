1a23d8552e0d02bbaa92e07aa670c7b5195337d1
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-06-04 20:14:29 +0200

    hotfix: fix registration page

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -145,7 +145,8 @@ export default function AccountPage() {
                   if (!isDisabled) {
                     notifyChange();
                   }
-                }}>
+                }}
+              >
                 {isDisabled ? 'Change Your Data' : 'Save Changes'}
               </Button>
             </div>
