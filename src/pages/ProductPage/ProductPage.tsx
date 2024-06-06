091e96dd5fb1fcc84681e51d2d1a1961f3374366
mikhailau-pavel <119894065+mikhailau-pavel@users.noreply.github.com>
2024-06-06 14:10:35 +0300

    Feat/3-10: Image slider for products (#113)
    * feat: add image slider
    
    * feat: add proper image enlargment in modal window
    
    * feat: add zoom cursor for slider images

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -4,11 +4,13 @@ import { getProductData } from '../../api/products';
 import { ProductData } from '../../store/types/products';
 import ImageModal from '../../components/ImageModal/ImageModal';
 import styles from './styles.module.css';
+import ImageSlider from '../../components/ImageSlider/ImageSlider';
 
 export default function ProductPage() {
   const { productId } = useParams();
   const [product, setProduct] = useState<ProductData | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -21,15 +23,15 @@ export default function ProductPage() {
     fetchProduct();
   }, [productId]);
 
-  const handleImageClick = () => {
-    setIsModalOpen(true);
-  };
+  const handleImageClick = (imageUrl: string) => {
+    setModalImage(imageUrl);
+    setIsModalOpen(true);  };
 
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  const { productName, productDescription, productImage, fullPrice, discountedPrice } = useMemo(() => {
+  const { productName, productDescription, productImages, fullPrice, discountedPrice } = useMemo(() => {
     if (!product || (product && !product.masterData) || !product.masterData.current.masterVariant.prices[0]) {
       return {};
     }
@@ -38,6 +40,7 @@ export default function ProductPage() {
     const name = product.masterData.current.name['en-US'];
     const description = product.masterData.current.description['en-US'];
     const image = product.masterData.current.masterVariant.images[0].url;
+    const images = product.masterData.current.masterVariant.images.map((image) => image.url);
     const price = product.masterData.current.masterVariant.prices[0];
     const fullPrice = price.value.centAmount / 100;
     const discountedPrice = price.discounted ? price.discounted.value.centAmount / 100 : undefined;
@@ -47,6 +50,7 @@ export default function ProductPage() {
       productName: name,
       productDescription: description,
       productImage: image,
+      productImages: images,
       fullPrice,
       discountedPrice
     };
@@ -62,9 +66,8 @@ export default function ProductPage() {
 
   return (
     <main className={styles.mainBlock}>
-      {productImage ? (
-        <img className={styles.productImg} src={productImage} alt={productName} onClick={handleImageClick} />
-      ) : null}
+      {productImages ? <ImageSlider images={productImages} onImageClick={handleImageClick}/> : null}
+      
       <div className={styles.informationBlock}>
         <div className={styles.contentBlock}>
           <h1 className={styles.h1}>{productName}</h1>
@@ -75,7 +78,7 @@ export default function ProductPage() {
           <p>{productDescription}</p>
         </div>
       </div>
-      {isModalOpen ? <ImageModal imageUrl={productImage} onClose={closeModal} /> : null}
+      {isModalOpen ? <ImageModal imageUrl={modalImage} onClose={closeModal} /> : null}
     </main>
   );
 }
