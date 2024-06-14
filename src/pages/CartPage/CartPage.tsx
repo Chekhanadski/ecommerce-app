36b2de830e8c74aebf8555fb3ed12dddf8322138
v-m-dev <morozvitalii2403@gmail.com>
2024-06-14 15:51:25 +0300

    feat: add message under title

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -15,7 +15,8 @@ export default function CartPage() {
       {isCartEmpty ? (
         <div>
           <img className={styles.emptyCartImg} src={cartImg} alt="Empty Cart" />
-          <h2 className={styles.emptyCartMessage}>Your cart is empty</h2>
+          <h2 className={styles.emptyCartTitle}>Your cart is empty</h2>
+          <p className={styles.emptyCartMessage}>Continue shopping to add items to your cart.</p>
         </div>
       ) : (
         // products go here
