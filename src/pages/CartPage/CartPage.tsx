f84cb2fb24fd94b10047c8b27a35f9f2c5fd87c6
v-m-dev <morozvitalii2403@gmail.com>
2024-06-14 15:35:36 +0300

    feat: add empty cart message

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
+import cartImg from '../../assets/icons/empty-cart.png';
 
 export default function CartPage() {
-  return <main className={styles.temporaryClass}>This is Cart Page</main>;
+  const [isCartEmpty, setIsCartEmpty] = useState(true);
+
+  useEffect(() => {
+    // if no new products loaded leave it as true
+    setIsCartEmpty(true);
+  }, []);
+
+  return (
+    <main className={styles.cartComponent}>
+      {isCartEmpty ? (
+        <div>
+          <img className={styles.emptyCartImg} src={cartImg} alt="Empty Cart" />
+          <h2 className={styles.emptyCartMessage}>Your cart is empty</h2>
+        </div>
+      ) : (
+        // products go here
+        'Your cart is not empty'
+      )}
+    </main>
+  );
 }
