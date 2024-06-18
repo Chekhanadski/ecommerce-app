92fcf7698e96f40088090005b89cddf67e74b532
Chekhanadski <115250960+Chekhanadski@users.noreply.github.com>
2024-06-18 23:02:27 +0200

    Issue RSS-ECOMM-4_18: Implement "Clear Shopping Cart" Button (10 points)  (#125)
    * feat: add "clear shopping cart" button
    
    * refactor: refactor CartPage
    
    * refactor: refactor styles CartPage
    
    * refactor: refactor CartPage code
    
    * Issue RSS-ECOMM-4_11: Modify Quantity of Products in Cart (15 points) (#124)
    
    * feat: add modify products quantity
    
    * Issue RSS-ECOMM-4_07: Add Product to Cart Functionality for Non-Duplicated Products (15 points) (#127)
    
    * chore: disabled rule eslint react/button-has-type
    
    * feat: add product to cart Functionality for non-duplicated products
    
    * fix: fix modal's state
    
    * Issue RSS-ECOMM-4_08: Remove Product from Cart Functionality (15 points) (#129)
    
    * feat: add button 'remove from cart'
    
    * feat: add modal window
    
    * refactor: refactor css
    
    * Issue RSS-ECOMM-4_16: Implement Promo Code Application Feature (15 points) (#130)
    
    * feat: add function promo code application
    
    * feat: add modal window for promo code error
    
    * refactor: refactor CartPage code

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
-import { getUserCart, getAnonymousCart, removeLineItem } from '../../api/cart';
+import {
+  getUserCart,
+  getAnonymousCart,
+  removeLineItem,
+  clearCart,
+  updateLineItemQuantity,
+  applyDiscountCode
+} from '../../api/cart';
 import { Cart, LineItem } from '../../store/types/cart';
 import cartImg from '../../assets/icons/empty-cart.png';
 import Button from '../../components/Button/Button';
 import { StoreContext } from '../../store/store';
+import Modal from '../../components/Modal/Modal';
 
 export default function CartPage() {
   const [cart, setCart] = useState<Cart | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [promoCode, setPromoCode] = useState<string>('');
   const [error, setError] = useState<string | null>(null);
   const { setStore } = useContext(StoreContext);
+  const [isPromoCodeValid, setIsPromoCodeValid] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchCart = async () => {
@@ -36,15 +47,21 @@ export default function CartPage() {
     fetchCart();
   }, [setStore]);
 
+  const handleClearCart = async () => {
+    try {
+      await clearCart();
+      setCart(null);
+      setStore((prevStore) => ({ ...prevStore, cartItemCount: 0 }));
+      setIsModalOpen(false);
+    } catch (error) {
+      setError(`Failed to clear cart: ${error}`);
+    }
+  };
+
   const handleRemoveFromCart = async (lineItemId: string) => {
     const customerId = localStorage.getItem('customerId');
     try {
-      let updatedCart;
-      if (customerId) {
-        updatedCart = await removeLineItem(lineItemId);
-      } else {
-        updatedCart = await removeLineItem(lineItemId, true);
-      }
+      const updatedCart = await removeLineItem(lineItemId, !customerId);
       setCart(updatedCart);
       const itemCount = updatedCart ? updatedCart.lineItems.reduce((count, item) => count + item.quantity, 0) : 0;
       setStore((prevStore) => ({ ...prevStore, cartItemCount: itemCount }));
@@ -53,6 +70,33 @@ export default function CartPage() {
       }
     } catch (error) {
       setError(`Failed to remove item from cart: ${error}`);
+    }
+  };
+
+  const handleQuantityChange = async (lineItemId: string, quantity: number) => {
+    if (quantity < 1) return;
+    const customerId = localStorage.getItem('customerId');
+    try {
+      const updatedCart = await updateLineItemQuantity(lineItemId, quantity, !customerId);
+
+      setCart(updatedCart);
+      const itemCount = updatedCart ? updatedCart.lineItems.reduce((count, item) => count + item.quantity, 0) : 0;
+      setStore((prevStore) => ({ ...prevStore, cartItemCount: itemCount }));
+    } catch (error) {
+      setError(`Failed to update item quantity: ${error}`);
+    }
+  };
+
+  const handleApplyPromoCode = async () => {
+    try {
+      const updatedCart = await applyDiscountCode(promoCode);
+      setCart(updatedCart);
+      setPromoCode('');
+      setIsPromoCodeValid(true);
+      const itemCount = updatedCart ? updatedCart.lineItems.reduce((count, item) => count + item.quantity, 0) : 0;
+      setStore((prevStore) => ({ ...prevStore, cartItemCount: itemCount }));
+    } catch (error) {
+      setIsPromoCodeValid(false);
     }
   };
 
@@ -72,7 +116,12 @@ export default function CartPage() {
         </div>
         <div className={styles.price}>{`Price: ${discountedPrice || fullPrice}€`}</div>
         <div className={styles.quantity}>
-          <input type="number" value={quantity} min="1" readOnly />
+          <input
+            type="number"
+            value={quantity}
+            min="1"
+            onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value, 10))}
+          />
         </div>
         <div className={styles.subtotal}>{`${totalPrice.centAmount / 100}€`}</div>
         <Button className="removeButton" type="button" onClick={() => handleRemoveFromCart(item.id)}>
@@ -119,11 +168,46 @@ export default function CartPage() {
         </div>
         {cart.lineItems.map(renderCartItem)}
 
-        {cart && cart.totalPrice && (
+        <div className={styles.buttonsBlock}>
+          <div className={styles.promoCodeBlock}>
+            <input
+              type="text"
+              value={promoCode}
+              onChange={(e) => setPromoCode(e.target.value)}
+              placeholder="Enter promo code"
+            />
+            <Button className="inputButton" type="button" onClick={handleApplyPromoCode}>
+              Apply
+            </Button>
+          </div>
+
+          <div className={styles.clearCartBlock}>
+            <Button className="clearCartButton" type="button" onClick={() => setIsModalOpen(true)}>
+              Clear Shopping Cart
+            </Button>
+          </div>
+        </div>
+
+        {cart.totalPrice && (
           <div className={styles.totalPrice}>{`Total Cart Price: ${cart.totalPrice.centAmount / 100}€`}</div>
         )}
         <Button type="button">Order</Button>
       </div>
+      {!isPromoCodeValid && (
+        <Modal
+          isOpen={!isPromoCodeValid}
+          onClose={() => setIsPromoCodeValid(true)}
+          title="Invalid Promo Code"
+          message="The promo code you entered is invalid or does not exist."
+        />
+      )}
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={handleClearCart}
+        title="Clear Cart"
+        message="Are you sure you want to clear your cart?"
+      />
     </main>
   );
 }
